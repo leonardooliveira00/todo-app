d64713d63cbddf723679666afe1a8c512a704c6e
Leonardo <leodev00@outlook.com>
2026-05-12 13:01:03 -0300

    security: added remote url env and a proxy url env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${process.env.REMOTE_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
